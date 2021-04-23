b2b5f461f12af4a43befa5827e3dbea557a2a98c
Kathleen Kong <kathleenkong@berkeley.edu>
2021-04-23 12:24:10 -0700

    completed code

--- project-sp21-skeleton-master/solver.py
+++ project-sp21-skeleton-master/solver.py
@@ -38,27 +38,70 @@ def solve(G):
     
     shortest_path_vertices = nx.dijkstra_path(G, 0, len(G.nodes) - 1)
 
-    shortest_path_edges = []
-    for i in range(len(shortest_path_vertices) - 1): # [0, 1, 2, 5, 3, 4]
-        edge_weight = G[shortest_path_vertices[i]][shortest_path_vertices[i+1]]['weight']
-        shortest_path_edges.append((shortest_path_vertices[i], shortest_path_vertices[i+1], edge_weight))
+    # shortest_path_edges = []
+    # for i in range(len(shortest_path_vertices) - 1): # [0, 1, 2, 5, 3, 4]
+    #     edge_weight = G[shortest_path_vertices[i]][shortest_path_vertices[i+1]]['weight']
+    #     shortest_path_edges.append((shortest_path_vertices[i], shortest_path_vertices[i+1], edge_weight))
 
-    shortest_path_edges.sort(key = lambda x: x[2], reverse = False)
+
+    shortest_path_edges_and_vertices = []
+    for i in range(len(shortest_path_vertices)): # [0, 1, 2, 5, 3, 4]
+    
+        # Safety check for i+1 case for edges
+        if i + 1 < len(shortest_path_vertices):
+            edge_weight = G[shortest_path_vertices[i]][shortest_path_vertices[i+1]]['weight']
+            shortest_path_edges_and_vertices.append((shortest_path_vertices[i], shortest_path_vertices[i+1], edge_weight))
+
+        # Looping through edges in Dijkstras to get edge weights + calculate heuristic
+        v = shortest_path_vertices[i]
+        vertex_weight = 0
+        count = 0
+        for edge in edges: 
+            if edge[0] == v or edge[1] == v:
+                vertex_weight +=  G[edge[0]][edge[1]]['weight']
+                count += 1
+        if count > 0:
+            vertex_weight = vertex_weight / count
+        shortest_path_edges_and_vertices.append((v, None, vertex_weight))
+
+    # Sorting all weights in ascending order
+    shortest_path_edges_and_vertices.sort(key = lambda x: x[2], reverse = False)
+    
+
+    # print(shortest_path_edges_and_vertices)
     # min_edge_weight = min(shortest_path_edges, key = lambda x: x[2]) #(A, B, weight)
 
     # best_path = shortest_path_edges
     # best_weights = sum(shortest_path_edges, key = lambda x: x[2])
 
-
     c = []
     k = []
-    for A, B, weight in shortest_path_edges:
+    for A, B, weight in shortest_path_edges_and_vertices:
+        # print("c =", c)
+        # print("k =", k)
+        if max_k <= 0 or max_c <= 0:
+            return c, k
         G_copy = G.copy()
-        G_copy.remove_edge(A, B) #args: u, v
+
+        if B == None: 
+            if A == 0 or A == len(nodes) - 1:
+                continue
+            if not nx.is_isolate(G, A):
+                continue
+            G_copy.remove_node(A)
+            # print("removed_node:", A)
+            c.append(A)
+            max_c = max_c - 1
         
-        k.append((A, B))
-        #checks if removing disconnects the graph
-        if is_valid_solution(G, c, k): 
+        else:
+            G_copy.remove_edge(A, B) #args: u, v
+            # print("removed edge:", (A, B))
+
+            k.append((A, B))
+            max_k = max_k - 1
+
+        # Checks if removing disconnects the graph
+        if is_valid_solution(G, c, k): #note this returns true when a node is disconnected
             new_c, new_k = solve(G_copy)
             c_copy = c + new_c
             k_copy = k + new_k
@@ -67,9 +110,17 @@ def solve(G):
                     c += new_c
                     k += new_k
         else:
-            k.remove((A, B))
-            
+            if B == None:
+                # print("added back node:", A)
+                c.remove(A)
+                max_c += 1
+            else:
+                # print("added back edge: ", (A, B))
+                k.remove((A, B))
+                max_k += 1
     return c, k
+
+
 
 # def is_better(G_copy, new_c, new_k, best_curr_weight):
 #     for e1, e2 in new_k:
@@ -116,23 +167,23 @@ def solve(G):
 # Usage: python3 solver.py test.in
 
 # RUN if you want to run ONE input:
-if __name__ == '__main__':
-    assert len(sys.argv) == 2
-    path = sys.argv[1]
-    G = read_input_file(path)
-    c, k = solve(G)
-    assert is_valid_solution(G, c, k)
-    print("Shortest Path Difference: {}".format(calculate_score(G, c, k)))
-    write_output_file(G, c, k, 'outputs/100.out')
+# if __name__ == '__main__':
+#     assert len(sys.argv) == 2
+#     path = sys.argv[1]
+#     G = read_input_file(path)
+#     c, k = solve(G)
+#     assert is_valid_solution(G, c, k)
+#     print("Shortest Path Difference: {}".format(calculate_score(G, c, k)))
+#     write_output_file(G, c, k, 'outputs/100.out')
 
 # RUN if you want to run ALL inputs:
 #For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
-# if __name__ == '__main__':
-#     inputs = glob.glob('inputs/*')
-#     for input_path in inputs:
-#         output_path = 'outputs/' + basename(normpath(input_path))[:-3] + '.out'
-#         G = read_input_file(input_path)
-#         c, k = solve(G)
-#         assert is_valid_solution(G, c, k)
-#         distance = calculate_score(G, c, k)
-#         write_output_file(G, c, k, output_path)
+if __name__ == '__main__':
+    inputs = glob.glob('inputs/small/*')
+    for input_path in inputs:
+        output_path = 'outputs/small' + basename(normpath(input_path))[:-3] + '.out'
+        G = read_input_file(input_path)
+        c, k = solve(G)
+        assert is_valid_solution(G, c, k)
+        distance = calculate_score(G, c, k)
+        write_output_file(G, c, k, output_path)
