66936164932fde48968b5cefcf68ef73365890a6
Kathleen Kong <kathleenkong@berkeley.edu>
2021-04-28 00:07:12 -0700

    commented

--- solver.py
+++ solver.py
@@ -136,32 +136,32 @@ def helper(G, m, t, max_c, max_k, first_time):
 # Usage: python3 solver.py test.in
 
 # RUN if you want to run ONE input:
-if __name__ == '__main__':
-    assert len(sys.argv) == 2
-    path = sys.argv[1]
-    G = read_input_file(path)
-    c, k = solve(G)
-    t = len(G.nodes) - 1
-    assert is_valid_solution(G, c, k, t)
-    print("Shortest Path Difference: {}".format(calculate_score(G, c, k, t)))
-    currScore = read_output_file(G, 'outputs/medium/medium-1.out', t)
-    print("currScore", currScore)
-    if currScore < calculate_score(G, c, k, t):
-        write_output_file(G, c, k, 'outputs/medium/medium-1.out')
+# if __name__ == '__main__':
+#     assert len(sys.argv) == 2
+#     path = sys.argv[1]
+#     G = read_input_file(path)
+#     c, k = solve(G)
+#     t = len(G.nodes) - 1
+#     assert is_valid_solution(G, c, k, t)
+#     print("Shortest Path Difference: {}".format(calculate_score(G, c, k, t)))
+#     currScore = read_output_file(G, 'outputs/medium/medium-1.out', t)
+#     print("currScore", currScore)
+#     if currScore < calculate_score(G, c, k, t):
+#         write_output_file(G, c, k, 'outputs/medium/medium-1.out')
 
 # RUN if you want to run ALL inputs:
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
-# if __name__ == '__main__':
-#     inputs = glob.glob('inputs/small/*')
-#     for input_path in inputs:
-#         output_path = 'outputs/small/' + basename(normpath(input_path))[:-3] + '.out'
-#         G = read_input_file(input_path)
-#         c, k = solve(G)
-#         t = len(G.nodes) - 1
-#         assert is_valid_solution(G, c, k, t)
-#         distance = calculate_score(G, c, k, t)
-#         print("distance", distance)
-#         currScore = read_output_file(G, output_path, t)
-#         print("currScore", currScore)
-#         if currScore < distance:
-#             write_output_file(G, c, k, output_path)
+if __name__ == '__main__':
+    inputs = glob.glob('inputs/small/*')
+    for input_path in inputs:
+        output_path = 'outputs/small/' + basename(normpath(input_path))[:-3] + '.out'
+        G = read_input_file(input_path)
+        c, k = solve(G)
+        t = len(G.nodes) - 1
+        assert is_valid_solution(G, c, k, t)
+        distance = calculate_score(G, c, k, t)
+        print("distance", distance)
+        currScore = read_output_file(G, output_path, t)
+        print("currScore", currScore)
+        if currScore < distance:
+            write_output_file(G, c, k, output_path)
