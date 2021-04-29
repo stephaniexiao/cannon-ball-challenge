8daf9b2db69beab55bc214bcc80fb2e41441c03f
EC2 Default User <ec2-user@ip-172-31-15-94.us-west-1.compute.internal>
2021-04-29 23:46:00 +0000

    Large-1 aws dennis

--- solver.py
+++ solver.py
@@ -143,9 +143,9 @@ def helper(G, m, t, max_c, max_k, first_time):
 # RUN if you want to run ALL inputs:
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
 if __name__ == '__main__':
-    inputs = glob.glob('inputs/small/*')
+    inputs = glob.glob('inputs/large/*')
     for input_path in inputs:
-        output_path = 'outputs/small/' + basename(normpath(input_path))[:-3] + '.out'
+        output_path = 'outputs/large/' + basename(normpath(input_path))[:-3] + '.out'
         G = read_input_file(input_path)
         c, k = solve(G)
         t = len(G.nodes) - 1
